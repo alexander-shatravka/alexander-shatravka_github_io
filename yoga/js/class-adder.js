cfc4088822d4b88f4d90cee0ae93160bb1c25439
Alexander Shatraka <alexander.shatraka@gmail.com>
2018-03-26 22:48:34 +0300

    fix contact list

--- yoga/js/class-adder.js
+++ yoga/js/class-adder.js
@@ -3,5 +3,6 @@ $(function() {
     $(".nav li a").click(function() {
         $(".nav li a").removeClass("active");
         $(this).toggleClass("active");
+        $('.menu-icon-close').removeClass('.menu-icon-close')
     });
 })
