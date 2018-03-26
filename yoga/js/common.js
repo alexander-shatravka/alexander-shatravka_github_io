6d8ea2bb7254346177552941a57ad2b7cccf5080
Alexander Shatraka <alexander.shatraka@gmail.com>
2018-03-26 17:07:05 +0300

    add project, fix yoga js

--- yoga/js/common.js
+++ yoga/js/common.js
@@ -1,14 +1,24 @@
-
 var menu = document.querySelector('.nav');
 var showMenu = document.querySelector('.menu-icon');
 var darkSheet = document.querySelector('.hide-dark-sheet');
 var wrapper = document.querySelector('#wrapper');
+
 
 showMenu.onclick = function () {
     menu.classList.toggle('nav-show');
     this.classList.toggle('menu-icon-close');
     darkSheet.classList.toggle('toggle-dark-sheet');
     wrapper.classList.toggle('fixed');
+    var navLi = document.querySelectorAll('.nav li a');
+
+    for (var i = 0; i < navLi.length; i++){
+        navLi[i].onclick = function () {
+            menu.classList.remove('nav-show');
+            this.classList.remove('menu-icon-close');
+            darkSheet.classList.remove('toggle-dark-sheet');
+            wrapper.classList.remove('fixed');
+        }
+    }
 }
 
 
