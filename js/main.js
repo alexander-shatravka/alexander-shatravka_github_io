f6b711cc88514f3f90150919d5256529f07e8c34
Alexander Shatravka <shatravgo2@gmail.com>
2018-05-02 15:41:42 +0300

    hide sidebar on click around it

--- js/main.js
+++ js/main.js
@@ -3,13 +3,22 @@ let showSidebar = $('.me');
 let hideSidebar = $('.close');
 let body = $('body');
 
-showSidebar.click(function (event) {
-    let target = event.target;
-    sidebar.addClass('show-sidebar');
-    body.addClass('scroll-off');
-})
-
-hideSidebar.click(function () {
+function hideS(){
     sidebar.removeClass('show-sidebar');
     body.removeClass('scroll-off');
-})
+}
+
+function showS(){
+    sidebar.addClass('show-sidebar');
+    body.addClass('scroll-off');
+
+    $(document).mouseup(function (e) {
+        var container = $(".sidebar");
+        if (container.has(e.target).length === 0 && e.target.className !== 'sidebar show-sidebar'){
+            hideS();
+        }
+    });
+}
+
+showSidebar.click(showS);
+hideSidebar.click(hideS);
