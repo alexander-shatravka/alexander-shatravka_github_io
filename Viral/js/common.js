3510ae4ed54d1d0c39be7d3b39efc8de72702f02
Alexander Shatravka <shatravgo2@gmail.com>
2018-09-25 11:46:58 +0300

    a

--- Viral/js/common.js
+++ Viral/js/common.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
     dotColor: '#394116',
-    lineColor: '#394116'
+    lineColor: '#394116',
+    density: 10000,
+    curvedLines: false,
+    parallaxMultiplier: 10,
+    particleRadius: 5,
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
